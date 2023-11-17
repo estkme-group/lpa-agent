0d9746dc3359fb457be5c021ffbed0ee379da9df
Damon To <damon.tu@outlook.com>
2023-11-17 17:02:43 +0800

    fix: don't ignore lpac

---
+++ lpac/types.go
@@ -0,0 +1,37 @@
+package lpac
+
+import "encoding/json"
+
+type lpaResponse struct {
+	Type    string      `json:"type"`
+	Payload *lpaPayload `json:"payload"`
+}
+
+type lpaPayload struct {
+	Code    int             `json:"code"`
+	Message string          `json:"message"`
+	Data    json.RawMessage `json:"data"`
+}
+
+type Information struct {
+	EID         string `json:"eid"`
+	DefaultSMDS string `json:"default_smds"`
+	DefaultSMDP string `json:"default_smdp,omitempty"`
+}
+
+type Profile struct {
+	ICCID               string       `json:"iccid"`
+	ISDPAID             string       `json:"isdpAid"`
+	DisplayName         string       `json:"profileNickname,omitempty"`
+	BuiltinName         string       `json:"profileName"`
+	State               ProfileState `json:"profileState"`
+	Class               ProfileClass `json:"profileClass"`
+	ServiceProviderName string       `json:"serviceProviderName"`
+}
+
+type Notification struct {
+	Index      int    `json:"seqNumber"`
+	Management int    `json:"profileManagementOperation"`
+	Address    string `json:"notificationAddress"`
+	ICCID      string `json:"iccid"`
+}
